0cbf898e11ab69c4201614488d411bbd8c013b9c
SkyyySi <53921371+SkyyySi@users.noreply.github.com>
2025-05-17 22:54:10 +0200

    Update - 2025-05-17T22:54:10+02:00

--- src/shared/MyLib/lib.c
+++ src/shared/MyLib/lib.c
@@ -3,10 +3,6 @@
 
 #define DLLEXPORT __attribute__((visibility("default")))
 
-/// FIXME: This is actually the API version of the recomp tool, not the mod!
-///
-/// This number should be reflected in the filename of your compiled shared
-/// library to allow for multiple versions to be installed simultaniously.
 DLLEXPORT const uint32_t recomp_api_version = 1;
 
 DLLEXPORT void MyLib_MyFunction(void) {
